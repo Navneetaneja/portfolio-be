480e73e0df612b3e462b34bd0768c90fedd5f53c
navneet <navneet0010.cse19@chitkara.edu.in>
2022-11-26 01:47:16 +0530

    Email Integration via sendgrid

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
-const express = require('express');
+const express = require("express");
 const PORT = 9045;
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SEND_API_KEY);
 
 let corsOptions = {
-    origin: ['http://localhost:3000', 'https://navneet-aneja-dev.netlify.app', 'https://navneet-aneja.netlify.app']
-}
+    origin: [
+        "http://localhost:3000",
+        "https://navneet-aneja-dev.netlify.app",
+        "https://navneet-aneja.netlify.app",
+    ],
+};
 
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", function(req,res){
-    return res.send("hello");
-})
+app.get("/", function (req, res) {
+    return res.send(
+        "<h1 style=color:red>HELLO FROM NAVNEET</h1><br/><h3 style=color:green>YOU CAN VISIT MY SITE BELOW</h3><br/><a style=color:blue href='https://navneet-aneja.netlify.app'>CLICK HERE</a>"
+    );
+});
 
-app.listen(process.env.PORT || PORT, function(err){
-    if(err){
+app.post("/send-email", function (req, res) {
+    const data = req.body;
+    const msg = {
+        to: "navneet.aneja.portfolio@gmail.com", // Change to your recipient
+        from: "navneetaneja2001@hotmail.com", // Change to your verified sender
+        subject: "Message From User In Portfolio",
+        html: `<h3>Message : ${data?.message}</h3><br/><h3>Email: ${data?.email}</h3><br/><h3>Name: ${data?.name}</h3>`,
+    };
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log("Email sent");
+            return res.status(200);
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500);
+        });
+});
+
+app.get("/**", function (req, res) {
+    return res.redirect("/");
+});
+
+app.listen(process.env.PORT || PORT, function (err) {
+    if (err) {
         console.log("Error in starting server", err);
         return;
     }
     return console.log("server started", PORT);
-})
+});
